4d6f3c12cdd6d60a984ea0905b91291b7354296b
Michael Stone <ms5512@columbia.edu>
2020-02-12 14:19:20 -0500

    Update proxy.py
    Added the server send and recv code and client code

--- PA1/proxy.py
+++ PA1/proxy.py
@@ -18,6 +18,31 @@ proxyServer.bind((serverAddr,serverPort))
 proxyServer.listen(5)
 print("Lisetning on the local host, port :",serverPort)
 
+while True:
+
+	# Accept incoming connection
+    clientSocket, clientAddr = proxyServer.accept()  # returns tuple
+
+    print("Connected to client on ", clientAddr)
+
+    while True:
+        try:
+            message = clientSocket.recv(BUFF_SIZE).decode()
+            if message:
+                print("Message from client: ", message)
+                clientToMainSocket.send(message.encode())
+                message = clientToMainSocket.recv(BUFF_SIZE).decode()
+                print("Message to client: ", message)
+            else:
+                readable, writable, errorable = select([],[], [clientSocket])
+                for s in errorable:
+                    s.close()
+                break
+        except:
+            clientSocket.close()
+            print("Connection closed")
+            break
+
 #Testing sending and reciving one message Seemd to work.
 
 '''testSentence = "test"
