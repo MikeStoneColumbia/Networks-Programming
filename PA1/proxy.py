c9545e7605289e015c60a28ef3e8cbbb8c8e5a40
Michael Stone <ms5512@columbia.edu>
2020-02-12 14:10:01 -0500

    Update proxy.py
    added proxy server connection

--- PA1/proxy.py
+++ PA1/proxy.py
@@ -5,11 +5,24 @@ import select
 BUFF_SIZE = 1024                                                                                                                                                                                                #Create server socket
 
 serverPort = str(sys.argv[1]) # getting the port number
+
+#Making connection to the back-end server
 serverAddr = ''  # localhost
 clientToMainSocket = socket(AF_INET, SOCK_STREAM)
-clientToMainSocket.connect(('',8888))
-testSentence = "test"
+clientToMainSocket.connect((serverAddr,8888))
+
+#Make proxy server
+
+proxyServer = socke((AF_INET,SOCK_STREAM))
+proxyServer.bind((serverAddr,serverPort))
+proxyServer.listen(5)
+print("Lisetning on the local host, port :",serverPort)
+
+#Testing sending and reciving one message Seemd to work.
+
+'''testSentence = "test"
 clientToMainSocket.send(testSentence.encode())
 print(clientToMainSocket.recv(BUFF_SIZE).decode())
 clientToMainSocket.close()
-#test pushing
+#test pushing ''' 
+
